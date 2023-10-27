4d7f6c13f8ddfd5587405121cc968d20dba63c89
Muzaffar Ahmed <contact@ahmedmuzaffar.com>
2023-10-27 22:20:00 +0530

    Tiktok OA Question 1

--- scripts/generator.py
+++ scripts/generator.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
-from random import randint as ri
+from random import randint as ri, sample, choice
 
-print(ri(1, 100))
+S = 4   # test size, n = 2S
+W = 20  # test width, proportional to max(a_i) - min(a_i)
+# you are free to modify above parameters
+print(S, ri(S * W / 2 - W, S * W / 2 + W))
+print(' '.join(map(str, [ri(1, W) for _ in range(S + S)])))
--- scripts/good.cpp
+++ scripts/good.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
+// Author: πα
+
+#include <bits/stdc++.h>
 using namespace std;
+#define long int64_t
 
 int main()
 {
-    int n;
-    cin >> n;
-    cout << n + n + (n == 50);
+    int n, k;
+    cin >> n >> k;
+    n += n;
+    vector<int> v(n);
+    for (int& it : v)
+        cin >> it;
+    sort(v.begin(), v.end());
+    int ans = INT_MAX;
+    do {
+        int cost = 0, cap = 0;
+        for (int i = 0; i < n; i += 2) {
+            if (v[i] > v[i + 1])
+                ++cost;
+            cap += v[i];
+        }
+        if (cap >= k)
+            ans = min(ans, cost);
+    } while (next_permutation(v.begin(), v.end()));
+    cout << (ans == INT_MAX ? -1 : ans) << '\n';
     return 0;
 }
